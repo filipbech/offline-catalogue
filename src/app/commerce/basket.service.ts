fb8a97c2af80454394834bdf82bc58928cdc85ff
Filip Bech-Larsen <filip@ano-t450.webdanmark.local>
2017-02-13 15:44:06 +0100

    Persisting basket to localstorage

--- src/app/commerce/basket.service.ts
+++ src/app/commerce/basket.service.ts
@@ -5,20 +5,31 @@ import { IProduct } from '../products/products.service';
 @Injectable()
 export class BasketService {
 
-	private basket: IProduct[] = [];
+	private basket: IProduct[];
 
 	private basketSubject = new BehaviorSubject(this.basket);
 	public basketChange$ = this.basketSubject.asObservable();
 
 	public addToBasket(item:IProduct) {
 		this.basket = [...this.basket, Object.assign({}, item)];
+		this.save();
 		this.basketSubject.next(this.basket);
 	}
 
 	public sendOrder() {
-		alert('sending order with '+ this.basket.length+' products. (check console)');
 		console.log(JSON.stringify(this.basket));
+		alert('sending order with '+ this.basket.length+' products. (check console)');
 		this.basket = [];
+		this.save();
+		this.basketSubject.next(this.basket);
+	}
+
+	private save() {
+		localStorage.setItem('basket',JSON.stringify(this.basket));
+	}
+
+	constructor() {
+		this.basket = JSON.parse(localStorage.getItem('basket') || '[]');
 		this.basketSubject.next(this.basket);
 	}
 
